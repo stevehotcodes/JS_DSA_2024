6d6d34b691ab2482e1c95ff3bbed1985600a5927
Stephen Ondieki <stevehotcodes@gmail.com>
2024-01-17 11:51:57 +0300

    random numbers

---
+++ randomNumbers.js
@@ -0,0 +1,15 @@
+// // //generate random number between 1 to 49
+
+let randomNumbers = [];
+
+for (let i = 1; i <= 6; i++) {
+    const min = 1;
+    const max = 50;
+
+    const randomInt = Math.floor(Math.random() * (max - min) + min);
+    randomNumbers.push(randomInt);
+}
+
+console.log(randomNumbers);
+
+
