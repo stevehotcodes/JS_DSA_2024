338da0513bc23c966c945351d0092764fd2201b1
Stephen Ondieki <stevehotcodes@gmail.com>
2024-01-17 11:09:22 +0300

    alternating pattern

--- alternatingPattern.js
+++ alternatingPattern.js
@@ -1,23 +1,17 @@
-let rows=5;
+let r=1;
+let asterisk=""
+let pattern="*"
+do{
+    asterisk+="*"
+    console.log(asterisk)
+    r++
 
-let pattern=""
-
-
-for(let i=1;i<=rows;rows++){
-    for( let num=1; num<=rows;num++){
-        pattern+="*"
-    }    
-    pattern+="\n";
-  
-}
-// console.log(pattern);
-
-
-for(let n=1;n<=rows;n++){
-    for(let num=6;num>n;num--){
-        pattern+="*"
+}while(r<6)
+for(let n=1;n<=5;n++){
+        for(let num=6;num>n;num--){
+            pattern+="*"
+        }
+        pattern +="\n"
     }
-    pattern +="\n"
-}
+ console.log(pattern)   
 
-console.log(pattern)
