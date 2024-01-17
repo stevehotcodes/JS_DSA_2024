60bdd836ba8d3c969687445bb5c06517664fa1af
Stephen Ondieki <stevehotcodes@gmail.com>
2024-01-17 12:03:13 +0300

    added alterantives for genearting random numbers

--- randomNumbers.js
+++ randomNumbers.js
@@ -12,4 +12,39 @@ for (let i = 1; i <= 6; i++) {
 
 console.log(randomNumbers);
 
+//while loop alternative
+
+let j=1
+let randomNumbers1 = [];
+
+while(j<7){
+    const min = 1;
+    const max = 50;
+
+    const randomInt = Math.floor(Math.random() * (max - min) + min);
+    randomNumbers1.push(randomInt);
+
+    j++
+}
+console.log("while loop alterantive of generating random numbers",randomNumbers1);
+
+//do while alternative
+
+let k=1
+let randomNumbers2 = [];
+
+do{
+    const min = 1;
+    const max = 50;
+
+    const randomInt = Math.floor(Math.random() * (max - min) + min);
+    randomNumbers2.push(randomInt);
+    k++
+    
+
+}while(k<=6)
+console.log("do while loop alterantive of generating random numbers",randomNumbers2);
+
+
+
 
