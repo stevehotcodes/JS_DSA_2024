ab1671a2af8d0a21ab9af6bb91780ab6bc6fab7e
Stephen Ondieki <stevehotcodes@gmail.com>
2024-01-17 10:22:48 +0300

    js loops

--- invertedLPattern.js
+++ invertedLPattern.js
@@ -1,16 +1,46 @@
-let n=5 // number of rows
-let p ="" // pattern sequence;
-
-for(let i=1;i<=n;i++){
-    for(let j=6;j>n;j--){
-       
-        if(j==6){
-            p+="*"  
-            break
-        }
-
-    }
-    p+="*"
+let p=""
+let asterisk="*"
+let repeatedAsterisk=asterisk.repeat(6)
+// console.log(repeatedAsterisk);
+for(let i=1;i<=7;i++){
+    p+="*\n"
 
 }
+console.log(repeatedAsterisk);
 console.log(p);
+
+//output 
+// ******
+// *
+// *
+// *
+// *
+// *
+// *
+// *
+
+
+
+
+let pattern=""
+let asterisk1="*"
+let repeatedAsterisk1=asterisk1.repeat(6)
+// console.log(repeatedAsterisk);
+for(let i=1;i<=7;i++){
+    pattern+="      *\n"
+
+}
+console.log("this prints an inverted l asterisks pattern in the right");
+console.log(repeatedAsterisk1);
+console.log(pattern,"\n");
+
+
+//output
+// ******
+//       *
+//       *
+//       *
+//       *
+//       *
+//       *
+//       *
